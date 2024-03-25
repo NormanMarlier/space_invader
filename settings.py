446c93e05edfefa858f0965dbc9d5ce6817ea5e1
Norman <norman.marlier@gmail.com>
2024-03-25 11:48:26 +0100

    Settings of the game

---
+++ settings.py
@@ -0,0 +1,29 @@
+# Screen
+GAME_W: int = 600
+GAME_H: int = 600
+SCREEN_W: int = 960
+SCREEN_H: int = 720
+BACKGROUND_COLOR: tuple = (30, 30, 30)
+
+# Font
+FONTSIZE: int = 15
+
+# Framerate
+FRAMERATE: int = 60
+
+# Motion
+SPACECRAFT_SPEED: int = 5
+ALIEN_SPEED: int = 2
+ALIEN_MOVE_DOWN_SPEED: int = 1
+EXTRA_SPEED:int = 2
+LASER_SPEED: int = 5
+ALIEN_LASER_SPEED: int = 3
+
+# Laser
+LASER_COOLDOWN: int = 600
+
+# Health
+LIVES: int = 3
+
+# Save
+SAVE_FILE: str = "save.json"
